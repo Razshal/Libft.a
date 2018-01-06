dfa451fc4c1ac2e2473ac933eb0daedc1a2e4ff9
Razshal <fontenille.mathieu@gmail.com>
2018-01-06 15:09:42 +0100

    back home commit

--- ft_printflstcount.c
+++ ft_printflstcount.c
@@ -1,25 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_printflstcount.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/01/04 16:04:32 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/06 14:32:22 by mfonteni         ###   ########.fr       */
+/*   Created: 2018/01/06 13:50:33 by mfonteni          #+#    #+#             */
+/*   Updated: 2018/01/06 15:09:30 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/libft.h"
-#include <stdarg.h>
-/*
-int			ft_printf(const char *format, ...)
+
+int		ft_argcount(const char *format)
 {
-	int written;
-	t_plist *instructions_list;
+	int count;
+	int argcount;
 
-	written = 0;
-	instructions_list = parse_input(format, args[...]);
-
-	return (written);
-}*/
+	count = 0;
+	argcount = 0;
+	while (format[count])
+	{
+//Not finished, useless for now
+		while (format[count] && !ft_is_printf_type(format[count]))
+			count++;
+		if (ft_is_printf_type(format[count]))
+			argcount++;
+	}
+	return (argcount);
+}
