93f7eb8f0522c1d9de8470d4053ee78a96af3f68
Razshal <fontenille.mathieu@gmail.com>
2018-01-12 17:56:26 +0100

    no more abort, better perfs (less mallocs)

--- printf_functions/printf_flags_num.c
+++ printf_functions/printf_flags_num.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 16:30:25 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/12 16:53:46 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/12 17:55:49 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,23 +19,22 @@ static int	is_signed(char c)
 	return (0);
 }
 
-static char	*ft_straddchar(char *s1, int totheright, char c, int toadd)
+static char	*ft_straddchar(char *s1, int totheright, char c, size_t toadd)
 {
 	char	*newstr;
-	char	filler[2];
+	size_t	count;
 
 	newstr = NULL;
-	filler[0] = c;
-	filler[1] = '\0';
+	count = 0;
 	if (s1)
 	{
-		newstr = (ft_strnew(ft_strlen(s1) + toadd));
-		if (newstr == NULL)
+		if (!(newstr = (ft_strnew(ft_strlen(s1) + toadd))))
 			return (NULL);
 		if (!totheright)
 			ft_strcat(newstr, s1);
-		while (toadd < ft_strlen(newstr) + ft_strlen(s1))
-			ft_strcat(newstr, filler);
+		count = ft_strlen(newstr);
+		while (toadd > (count + ft_strlen(s1) - 1))
+			newstr[count++] = c;
 		if (totheright)
 			ft_strcat(newstr, s1);
 		free(s1);
@@ -52,11 +51,10 @@ void		printf_flags_num(t_plist *list)
 	str = list->arg;
 	rightalign = (ft_strchr(list->flag, '-') ? 0 : 1);
 	zeroorspace = list->precision == -1 && list->width ? '0' : ' ';
-	if (list->precision != -1 && (ft_strlen(str) < (size_t)list->precision))
-		str = ft_straddchar(str, 1, '0', list->precision - ft_strlen(str));
+	if (list->precision > -1 && (ft_strlen(str) < (size_t)list->precision))
+		str = ft_straddchar(str, 1, '0', (size_t)list->precision);
 	if (list->width != 0 && (size_t)list->width > ft_strlen(str))
-		str = ft_straddchar(str, rightalign, zeroorspace,
-				ft_strlen(str) - list->width);
+		str = ft_straddchar(str, rightalign, zeroorspace, (size_t)list->width);
 	if ((ft_strchr(list->flag, ' ') || ft_strchr(list->flag, '+'))
 				&& is_signed(list->type))
 		str = ft_straddchar(str, 1,
