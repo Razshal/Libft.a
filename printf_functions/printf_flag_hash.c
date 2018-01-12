56d1d47de1507ef4de61a3c4e0fa991c92d44ca0
Razshal <fontenille.mathieu@gmail.com>
2018-01-12 13:59:29 +0100

    afk lunch

--- printf_functions/printf_flag_hash.c
+++ printf_functions/printf_flag_hash.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 16:29:49 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/11 17:32:59 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/12 13:58:47 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,20 +20,20 @@ void	printf_flag_hash(t_plist *list)
 	leftalign = (ft_strchr(list->flag, '-') ? 1 : 0);
 	if (ft_strchr(list->flag, '#') && list->type == 'o')
 	{
-		temp = ft_strjoin("0", (char*)list->arg);
+		temp = ft_strjoin("0", list->arg);
 		ft_memdel(list->arg);
 		list->arg = temp;
 	}
 	else if (ft_strchr(list->flag, '#') && list->type == 'x')
 	{
-		temp = ft_strjoin("0x", (char*)list->arg);
+		temp = ft_strjoin("0x", list->arg);
 		ft_memdel(list->arg);
 		list->arg = temp;
 	}
 	else if (ft_strchr(list->flag, '#') && list->type == 'X')
 	{
-		temp = ft_strjoin("0X", (char*)list->arg);
-		ft_memdel(list->arg);
+		temp = ft_strjoin("0X", list->arg);
+		free(list->arg);
 		list->arg = temp;
 	}
 }
--- printf_functions/printf_flags_num.c
+++ printf_functions/printf_flags_num.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 16:30:25 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/12 12:38:38 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/12 13:54:42 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,33 +47,21 @@ static char	*ft_straddchar(char *s1, int totheright, char c)
 	return (newstr);
 }
 
-static void replace_space_by_zeroes(char *str)
-{
-	int count;
-
-	count = -1;
-	while (ft_ispace(str[++count]))
-		str[count] = '0';
-	count = ft_strlen(str);
-	while (ft_ispace(str[--count]))
-		str[count] = '0';
-}
-
 void		printf_flags_num(t_plist *list)
 {
 	int rightalign;
 	char *str;
 
 	str = list->arg;
-	rightalign = (ft_strchr(list->flag, '-') ? 1 : 0);
+	rightalign = (ft_strchr(list->flag, '-') ? 0 : 1);
 	while (list->precision != -1 && ft_strlen(str) < (size_t)list->precision)
 		str = ft_straddchar(str, 1, '0');
 	while (list->width != 0 && (size_t)list->width > ft_strlen(str))
-		str = ft_straddchar(str, rightalign, ' ');
-	if (list->precision == -1 && list->width)
-		replace_space_by_zeroes(str);
-	if ((ft_strchr(list->flag, ' ') || ft_strchr(list->flag, '+')) &&
-			is_signed(list->type))
+		str = ft_straddchar(str, rightalign,
+				(list->precision == -1 && list->width ? '0' : ' '));
+	if ((ft_strchr(list->flag, ' ') ||
+					ft_strchr(list->flag, '+')) && is_signed(list->type))
 		str = ft_straddchar(str, 1, (ft_strchr(list->flag, '+') ? '+' : ' '));
+	list->arg = str;
 	printf_flag_hash(list);
 }
