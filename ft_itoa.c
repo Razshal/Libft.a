e1b84902339fafd7fb9e82969e1d59868e220e23
Razshal <fontenille.mathieu@gmail.com>
2018-01-21 19:28:30 +0100

    solved bug width long_min

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/11 12:07:57 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/19 15:26:37 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/21 19:27:25 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +75,8 @@ char		*ft_umax_itoabase(int base, uintmax_t n, int isupper)
 	size = ft_umax_countdigit_base(n, base) - 1;
 	tab = base_table(isupper);
 	str = ft_strnew((size > 0 ? size : 1));
-	str[0] = '0';
+	if (str)
+		str[0] = '0';
 	while (str && n)
 	{
 		str[--size] = tab[n % base];
@@ -87,28 +88,26 @@ char		*ft_umax_itoabase(int base, uintmax_t n, int isupper)
 
 char		*ft_max_itoabase(int base, intmax_t n, int isupper)
 {
-	char	*str;
-	int		size;
-	int		checkneg;
-	char	*tab;
+	char		*str;
+	int			size;
 
-	checkneg = 0;
+	char		*tab;
+	uintmax_t	num;
+
+
 	size = ft_max_countdigit_base(n, base) - 1;
 	tab = base_table(isupper);
+	num = (n > 0 ? n : -n);
 	if (n < 0)
-	{
-		checkneg = 1;
-		n = -n;
 		size++;
-	}
 	str = ft_strnew((size > 0 ? size : 1));
-	str[0] = '0';
-	if (str && checkneg)
+	(str ? str[0] = '0' : 0);
+	if (str && n < 0)
 		str[0] = '-';
-	while (str && n)
+	while (str && num)
 	{
-		str[--size] = tab[n % base];
-		n = n / base;
+		str[--size] = tab[num % base];
+		num = num / base;
 	}
 	free(tab);
 	return (str);
