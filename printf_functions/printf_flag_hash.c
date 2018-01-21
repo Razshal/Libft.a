68dd5bd77c2a950e8982b5ae113d2f76efd57f37
Razshal <fontenille.mathieu@gmail.com>
2018-01-21 12:39:26 +0100

    solved %p bugs

--- printf_functions/printf_flag_hash.c
+++ printf_functions/printf_flag_hash.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/10 16:29:49 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/21 12:33:51 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/21 12:38:54 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void	printf_flag_hash(t_plist *list)
 	char	*temp;
 
 	temp = NULL;
-	if (ft_strchr(list->flag, '#'))
+	if (ft_strchr(list->flag, '#') || list->type == 'p')
 	{
 		if (ft_strchr(list->flag, '#') && list->type == 'o')
 			temp = ft_strjoin("0", list->arg);
-		 if (ft_strchr(list->flag, '#') &&
-				 (list->type == 'x' || list->type == 'p'))
+		 if ((ft_strchr(list->flag, '#') &&
+				 list->type == 'x') || list->type == 'p')
 			temp = ft_strjoin("0x", list->arg);
 		else if (ft_strchr(list->flag, '#') && list->type == 'X')
 			temp = ft_strjoin("0X", list->arg);
