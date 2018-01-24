782a0bc9a91f470f2852e82a49f42e3b0afc66e5
Razshal <fontenille.mathieu@gmail.com>
2018-01-24 12:31:23 +0100

    working on more os compatibility

--- printf_functions/printf_types.c
+++ printf_functions/printf_types.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/13 19:07:24 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/22 14:50:15 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/24 12:28:31 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,19 +42,19 @@ char		*printf_type_d(t_plist *list)
 	if (list->arg == 0)
 		return (checkifnull(list) ? NULL : ft_max_itoabase(10, 0, 0));
 	else if (check_hconv(list))
-		return (ft_max_itoabase(10, (short)list->arg, 0));
+		return (ft_max_itoabase(10, (short)(list->arg), 0));
 	else if (list->length[0] == 'h' && list->length[1] == 'h')
-		return (ft_max_itoabase(10, (char)list->arg, 0));
+		return (ft_max_itoabase(10, (char)(list->arg), 0));
 	else if (check_lconv(list) || list->type == 'D')
-		return (ft_max_itoabase(10, (long)list->arg, 0));
+		return (ft_max_itoabase(10, (long)(list->arg), 0));
 	else if (list->length[0] == 'l' && list->length[1] == 'l')
-		return (ft_max_itoabase(10, (long long)list->arg, 0));
+		return (ft_max_itoabase(10, (long long)(list->arg), 0));
 	else if (list->length[0] == 'j')
-		return (ft_max_itoabase(10, (intmax_t)list->arg, 0));
+		return (ft_max_itoabase(10, (intmax_t)(list->arg), 0));
 	else if (list->length[0] == 'z')
-		return (ft_max_itoabase(10, (size_t)list->arg, 0));
+		return (ft_max_itoabase(10, (size_t)(list->arg), 0));
 	else if (!(list->length[0]) && !(list->type == 'D'))
-		return (ft_max_itoabase(10, (int)list->arg, 0));
+		return (ft_max_itoabase(10, (int)(list->arg), 0));
 	return (NULL);
 }
 
@@ -70,19 +70,19 @@ char		*printf_type_unsigned(t_plist *list)
 	else if (list->type == 'p')
 		return (pointer(list));
 	else if (check_hconv(list))
-		return (ft_umax_itoabase(base, (unsigned short)list->arg, uppercase));
+		return (ft_umax_itoabase(base, (unsigned short)(list->arg), uppercase));
 	else if (list->length[0] == 'h' && list->length[1] == 'h')
-		return (ft_umax_itoabase(base, (unsigned char)list->arg, uppercase));
+		return (ft_umax_itoabase(base, (unsigned char)(list->arg), uppercase));
 	else if (check_lconv(list) || list->type == 'D')
-		return (ft_umax_itoabase(base, (unsigned long)list->arg, uppercase));
+		return (ft_umax_itoabase(base, (unsigned long)(list->arg), uppercase));
 	else if (list->length[0] == 'l' && list->length[1] == 'l')
 		return (ft_umax_itoabase(base,
-					(unsigned long long)list->arg, uppercase));
+					(unsigned long long)(list->arg), uppercase));
 	else if (list->length[0] == 'j')
-		return (ft_umax_itoabase(base, (uintmax_t)list->arg, uppercase));
+		return (ft_umax_itoabase(base, (uintmax_t)(list->arg), uppercase));
 	else if (list->length[0] == 'z')
-		return (ft_umax_itoabase(base, (size_t)list->arg, uppercase));
+		return (ft_umax_itoabase(base, (size_t)(list->arg), uppercase));
 	else if (!(list->length[0]))
-		return (ft_umax_itoabase(base, (unsigned int)list->arg, uppercase));
+		return (ft_umax_itoabase(base, (unsigned int)(list->arg), uppercase));
 	return (NULL);
 }
