a164d811aa04defe2f66afd0c481dac6b30c7cd2
Razshal <fontenille.mathieu@gmail.com>
2018-01-31 17:17:06 +0100

    print_split update

--- ft_print_split.c
+++ ft_print_split.c
@@ -6,7 +6,7 @@
 /*   By: mfonteni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/16 16:49:11 by mfonteni          #+#    #+#             */
-/*   Updated: 2018/01/25 16:01:08 by mfonteni         ###   ########.fr       */
+/*   Updated: 2018/01/31 17:16:50 by mfonteni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_print_split(char **tab)
 	int cursor;
 
 	cursor = 0;
-	while (tab[cursor] != 0)
+	while (tab[cursor])
 	{
 		ft_putstr(tab[cursor]);
 		ft_putchar('\n');
@@ -30,7 +30,7 @@ void	ft_print_split_fd(char **tab, int fd)
 	int cursor;
 
 	cursor = 0;
-	while (tab[cursor] != 0)
+	while (tab[cursor])
 	{
 		ft_putstr_fd(tab[cursor], fd);
 		ft_putchar_fd('\n', fd);
